37248b5862413efcdcdfb93e1441a79e1e518fdf
anujvishwakarma07 <anujvishwakarma7077@gmail.com>
2026-07-08 13:29:58 +0530

    style: configure global typography using Plus Jakarta Sans and JetBrains Mono

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
 import "@/styles/globals.css";
+import { Plus_Jakarta_Sans, JetBrains_Mono } from "next/font/google";
+
+const sans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+const mono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${sans.variable} ${mono.variable} font-sans`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
