a1e70285f438c9cf7b852b9f9efc499a41f4980a
anujvishwakarma07 <anujvishwakarma7077@gmail.com>
2026-07-08 11:27:21 +0530

    chore : configuration database connection and prisma schema for notice model

---
+++ prisma/migrations/20260708055511_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Notice" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "priority" TEXT NOT NULL,
+    "publishDate" TIMESTAMP(3) NOT NULL,
+    "image" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Notice_pkey" PRIMARY KEY ("id")
+);
